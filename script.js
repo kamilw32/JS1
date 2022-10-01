09fff79625c28f8abd35055f8933b8336daf0f7d
Kamil Wojcik <kamil.wojcik@microsoft.wsei.edu.pl>
2022-10-01 14:57:23 +0200

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("<h1 id=\"header\"></h2>")
+console.log("d:\\data\\file.js")
+let name = "Karol"
+console.log(`your namie is ${name.toUpperCase()}`)
+
+console.log(1 + "1")
+console.log(1 + "1")
+let age = 1
+let isActive = true
+let propoetis = Symbol('a')
+let n
+let m = null
